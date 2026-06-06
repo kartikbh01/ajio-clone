7be00af9c2b7d8b1100fc8c127d121dce9ef518d
kartikbh01 <kartik.bh@omnireach.com>
2026-06-06 08:40:00 +0530

    revamp: reconfigure routes and pages according to API endpoints

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Toaster } from "sonner"
+import { ThemeProvider } from "./components/theme-provider"
+import HomePage from "./pages/HomePage"
+import ProductListingPage from "./pages/ProductListingPage"
+import ProductDetailPage from "./pages/ProductDetailPage"
+import CartPage from "./pages/CartPage"
+import WishlistPage from "./pages/WishlistPage"
+import { Navbar } from "./components/navbar/Navbar"
+import { Footer } from "./components/common/Footer"
+
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col bg-background">
+      <Navbar />
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="ajio-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout><HomePage /></Layout>} />
+          <Route path="/products" element={<Layout><ProductListingPage /></Layout>} />
+          <Route path="/product/:id" element={<Layout><ProductDetailPage /></Layout>} />
+          <Route path="/cart" element={<Layout><CartPage /></Layout>} />
+          <Route path="/wishlist" element={<Layout><WishlistPage /></Layout>} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster position="bottom-right" richColors />
+    </ThemeProvider>
+  )
+}
+
+export default App
